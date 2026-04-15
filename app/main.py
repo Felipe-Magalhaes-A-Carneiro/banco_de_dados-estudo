40a7330cbeb8c9f78e94b6ec0d6eedff4a9fee14
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-04-15 14:03:04 -0300

    feat(): cria primeira tabela e seus atributos

--- app/main.py
+++ app/main.py
@@ -6,3 +6,4 @@ ROOT_PATH = Path(__file__).parent
 con = sqlite3.connect(ROOT_PATH / 'meu_banco_de_dados.db')
 cur = con.cursor()
 
+cur.execute("CREATE TABLE clientes(id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(200), email VARCHAR(150))")
