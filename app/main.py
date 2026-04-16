bade800a4a8589fb3a70fa436148bad46b148d52
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-04-16 13:17:34 -0300

    feat(): cria funcoes para criar tabela e inserir dados

--- app/main.py
+++ app/main.py
@@ -6,12 +6,13 @@ ROOT_PATH = Path(__file__).parent
 con = sqlite3.connect(ROOT_PATH / 'meu_banco_de_dados.db')
 cur = con.cursor()
 
-# Agora é necessário comentar o código abaixo para que o Python não tente criar a tabela novamente:
-# cur.execute("CREATE TABLE clientes(id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(200), email VARCHAR(150))")
+def criar_tabela(cur):
+    cur.execute("CREATE TABLE clientes(id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(200), email VARCHAR(150))")
+    con.commit()
+    
 
-# INSERINDO dados na tabela criada:
-data = ("Felipe", "felipe.araujo@mail.com")
-cur.execute('INSERT INTO clientes (nome, email) VALUES (?, ?);', (data))
+def inserir_dados(con, cur, nome, email):
+    data = (nome, email)
+    cur.execute('INSERT INTO clientes (nome, email) VALUES (?, ?);', (data))
+    con.commit()
 
-# Envia as alterações para o banco de dados:
-con.commit()
